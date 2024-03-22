5cd8b52e66b22fae58d498b8c553297652df519d
Pietra <87500549+ComputerSciencePassion@users.noreply.github.com>
2024-03-22 02:59:42 +0100

    commit

--- backend/backend.Data/obj/Debug/net8.0/backend.Data.AssemblyInfo.cs
+++ backend/backend.Data/obj/Debug/net8.0/backend.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb997ebbcb7eb6762dbc19cdabc701a74bb54199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb5d2101099d89e7e335f07b1b8c27275f121bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
