81aa79c61744b4e8caf888d44c60e156c6c9d181
Pietra323 <pietross771@gmail.com>
2024-04-01 06:04:05 +0200

    main

--- backend/backend.Data/obj/Debug/net8.0/backend.Data.AssemblyInfo.cs
+++ backend/backend.Data/obj/Debug/net8.0/backend.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687df0ab7cc5c4ae885003d5e8376fad5dfb75da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67d45687fb766d916945bb48167ad20fec3b2ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
